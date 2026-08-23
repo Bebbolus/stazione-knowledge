c299171d781b36800396034bf765307ecd22414f
Bebbolus <lambis.net@gmail.com>
2026-08-23 16:32:23 +0200

    fix: risolto React error 310 hook order

---
+++ docs/interactive/assets/swimlanesDiagram-VK2B7HYN-9BSOFMuA.js
@@ -0,0 +1,8 @@
+import"./src-DiyhtlPM.js";import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./chunk-DU6HZSFF-BK-vcbZ8.js";import"./chunk-75Z2AOVW-DogPy-Sv.js";import"./chunk-P2QGCYS3-BHiD-_dg.js";import"./chunk-PWAF6VOD-ColFxj2v.js";import"./chunk-GMAD6QVW-BeBw6-jA.js";import"./chunk-TEH6E4GO-CgSO0QJv.js";import"./chunk-OBVCFTLP-D17_PNAB.js";import"./index-DCBzfUia.js";import"./chunk-F27PBJKO-B0zaxEva.js";import"./chunk-XXDRQBXY-07j8Bflc.js";import"./chunk-POPQ4Y6H-Dpa3EqRg.js";import{r as t,t as n}from"./chunk-RHFEMEQ7-J497z-2U.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
