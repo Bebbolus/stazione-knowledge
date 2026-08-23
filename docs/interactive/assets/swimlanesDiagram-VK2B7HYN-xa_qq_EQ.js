a8bc5676b075be752bcfd181687a740ce92254d0
Bebbolus <lambis.net@gmail.com>
2026-08-23 12:02:16 +0200

    feat: rimossa generazione AI e aggiunto componente ProgressiveMindmap locale per tracciare il percorso concettuale

---
+++ docs/interactive/assets/swimlanesDiagram-VK2B7HYN-xa_qq_EQ.js
@@ -0,0 +1,8 @@
+import"./src-DiyhtlPM.js";import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./chunk-DU6HZSFF-CEwFcbk2.js";import"./chunk-75Z2AOVW-Bzle35In.js";import"./chunk-P2QGCYS3-ClWrpHbN.js";import"./chunk-PWAF6VOD-BgfkE-3v.js";import"./chunk-GMAD6QVW-D_Zxlw6G.js";import"./chunk-TEH6E4GO-DWZQ-io6.js";import"./chunk-OBVCFTLP-XLcuD6uC.js";import"./index-CqENf2xV.js";import"./chunk-F27PBJKO-Brs-nFxl.js";import"./chunk-XXDRQBXY-07j8Bflc.js";import"./chunk-POPQ4Y6H-ChUEgRdf.js";import{r as t,t as n}from"./chunk-RHFEMEQ7-O2Wj2Pro.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
